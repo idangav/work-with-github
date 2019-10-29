53f96590795383030a1d6cdc01dfa9617ef723bf
idangav <idan.gav86@gmail.com>
2019-10-29 20:03:02 +0200

    Delete build.groovy

--- build.groovy
+++
@@ -1,9 +0,0 @@
-properties([pipelineTriggers([pollSCM('*/2 * * * *')])])
-node {
-    stage ("checkout reposetory"){
-        git branch: 'develop', url: 'https://github.com/idangav/work-with-github.git'
-    }
-    stage ("Hello world"){
-        echo "Hello World!"
-    }
-}
